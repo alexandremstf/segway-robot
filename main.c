4bff4ea384d2d8899f4108ebf7c27f340894a1ac
Alexandre <alexandremstf@gmail.com>
2018-07-12 17:32:00 -0300

    PWM funcionando

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #define BAUD 38400
 #define MYUBRR 16000000U / 16 / BAUD-1
+#define CONFIG_POTB DDRB |= 0x01
+#define CONFIG_POTD DDRD |= 0xF0
 
 #include "i2c.h"										/* Include I2C file */
 #include "usart.h"										/* Include USART file */
 #include "mpu6050.h"									/* Include MPU6050 register define file */
 #include <util/delay.h>									/* Include delay header file */
 #include <avr/interrupt.h>
+#include <stdlib.h>
 
 double dt = 0.01;
 double angle_y = 0;
@@ -13,11 +16,17 @@ double angle_y = 0;
 double get_angle();
 void controller();
 void config_timer_interrupt();
+void configPWM();
+void set_PWM(int16_t, uint8_t);
 
 void main() {
+	CONFIG_POTB;
+	CONFIG_POTD;
+
 	i2c_init();
 	usart_init(MYUBRR);
 	config_timer_interrupt();
+	configPWM();
 
 	mpu6050_init(0x68);
 	mpu6050_init(0x69);
@@ -26,6 +35,16 @@ void main() {
 }
 
 void controller(){
+	/*
+	if (get_angle() < 0) {
+		set_PWM(120, 1);
+		set_PWM(120, 2);
+	} else {
+		set_PWM(-120, 1);
+		set_PWM(-120, 2);
+	}
+	*/
+
 	usart_transmit_double( get_angle() );
 	usart_transmit('\n');
 }
@@ -37,9 +56,50 @@ double get_angle(){
 	double gyro_y = (read_gyro_y(0x68) + read_gyro_y(0x69)) / 2 ;
 
 	double pitch = atan(-acceleration_z / sqrt(acceleration_y * acceleration_y + acceleration_x * acceleration_x)) * (180.0 / M_PI);
-	angle_y = 0.7 * (angle_y + gyro_y * dt) + 0.3 * pitch;
+	angle_y = 0.8 * (angle_y + gyro_y * dt) + 0.2 * pitch;
 
 	return angle_y;
+}
+
+//TIMER 2 for Fast PWM with 31.7kHz
+
+void configPWM() {
+	//Set ports PD3 and PB3 as output
+	DDRB |= 1 << PB3;
+	DDRD |= 1 << PD3;
+
+	//Turn on Fast PWM mode
+	TCCR2A |= (1 << COM2A1) | (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
+	TCCR2B |= (1 << CS20);
+
+	//STBY PIN TO HIGH
+	PORTD |= 0x40;
+}
+
+void set_PWM(int16_t pwm, uint8_t pin) {
+	//Output compare register set for PWM level
+	//8bits (0 to 255)
+
+	if (pin == 1) {
+		if (pwm > 0) {
+			PORTD |= 0x20;
+			PORTD &= ~0x10;
+		} else {
+			PORTD &= ~0x20;
+			PORTD |= 0x10;
+		}
+		OCR2B = abs(pwm);
+
+	} else if (pin == 2) {
+		if (pwm > 0) {
+			PORTD |= 0x80;
+			PORTB &= ~0x01;
+		} else {
+			PORTD &= ~0x80;
+			PORTB |= 0x01;
+		}
+		OCR2A = abs(pwm);
+	}
 }
 
 void config_timer_interrupt() {
